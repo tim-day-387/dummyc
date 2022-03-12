92b9b4aea2216762380eecaa0e23c1fba1df5246
Timothy Day <tday141@gmail.com>
2022-03-12 17:31:47 -0500

    Added testing framework, simply implementation of output_type

--- src/data.rs
+++ src/data.rs
@@ -32,11 +32,45 @@ impl Data {
 
     // Determine output type
     fn output_type(&mut self) {
-	
+	let char_vec:Vec<char> = self.plain_text.chars().collect();
+	let first = 0;
+	let last = char_vec.len()-1;
+
+	// Series of cases to find type
+	if char_vec.get(first).expect("First char missing!") == &'"' && char_vec.get(last).expect("First char missing!") == &'"' {
+	    self.output_type = "string".to_string();
+	} else {
+	    self.output_type = "unresolved".to_string();
+	}
     }
 
     // Find text to be printed out
-    fn get_print_out)&mut self) {
+    fn get_print_out(&mut self) {
 
+    }
+}
+
+// Testing methods
+#[cfg(test)]
+mod test {
+    // File Imports
+    use data::*;
+    
+    // Testing output_type()
+    #[test]
+    fn type_1() {
+	let mut given:Data = Data::new("\"This is a test\"".to_string());
+	given.output_type();
+
+	assert_eq!("string".to_string(), given.output_type);
+    }
+
+    // Testing output_type()
+    #[test]
+    fn type_2() {
+	let mut given:Data = Data::new("This is another, different test.".to_string());
+	given.output_type();
+
+	assert_eq!("unresolved".to_string(), given.output_type);
     }
 }
