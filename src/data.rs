227bbf84adadbd5cb8d29bc28e9f656153257503
Tim Day <tday141@gmail.com>
2022-03-12 22:49:58 -0500

    Implemented more of the data class

--- src/data.rs
+++ src/data.rs
@@ -1,11 +1,14 @@
 // Data module
 #![forbid(unsafe_code)]
 
+// General Imports
+use std::collections::HashMap;
+
 // Data struct
 pub struct Data {
-    plain_text:String,
-    output_type:String,
-    print_out_text:String,
+    pub plain_text:String,
+    pub output_type:String,
+    pub print_out_text:String,
 }
 
 // Data implementation
@@ -22,16 +25,17 @@ impl Data {
 
     // Simplify data output to one which can be stored and printed out
     pub fn simplify(&mut self) {
-
+	self.find_output_type();
+	self.get_print_out();
     }
     
     // Resolve any unresolved operations in the expression
     fn resolve(&mut self) {
-
+	
     }
 
     // Determine output type
-    fn output_type(&mut self) {
+    fn find_output_type(&mut self) {
 	let char_vec:Vec<char> = self.plain_text.chars().collect();
 	let first = 0;
 	let last = char_vec.len()-1;
@@ -44,9 +48,30 @@ impl Data {
 	}
     }
 
+    // Get variable value
+    fn get_var_value(&mut self, mut vars:HashMap<String, Data>) {
+	let mut var_value:&Data = &Data::new("".to_string());
+	
+	match vars.get(&self.plain_text) {
+	    Some(value)=> var_value = value,
+	    _=> println!("ERROR VAL"),
+	}
+
+	// Simplify later
+	self.plain_text = var_value.plain_text.clone();
+	self.output_type = var_value.output_type.clone();
+	self.print_out_text = var_value.print_out_text.clone();
+    }
+
     // Find text to be printed out
     fn get_print_out(&mut self) {
-
+	// Series of cases to get output_string
+	if self.output_type == "string".to_string() {
+	    // Use the plain text but remove parans
+	    self.print_out_text = self.plain_text.clone();
+	    self.print_out_text.pop();
+	    self.print_out_text.remove(0);
+	}
     }
 }
 
@@ -60,7 +85,7 @@ mod test {
     #[test]
     fn type_1() {
 	let mut given:Data = Data::new("\"This is a test\"".to_string());
-	given.output_type();
+	given.find_output_type();
 
 	assert_eq!("string".to_string(), given.output_type);
     }
@@ -69,7 +94,7 @@ mod test {
     #[test]
     fn type_2() {
 	let mut given:Data = Data::new("This is another, different test.".to_string());
-	given.output_type();
+	given.find_output_type();
 
 	assert_eq!("unresolved".to_string(), given.output_type);
     }
