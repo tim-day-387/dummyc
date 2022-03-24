477062e4a53a240a8020a1f592572e7b70324604
Tim Day <tday141@gmail.com>
2022-03-24 02:05:50 -0400

    Made the lexer space agnostic

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,7 +3,7 @@
 
 // Perform all lexer commands
 pub fn perform_lexing(file_string:String) -> (Vec<String>, Vec<String>) {
-    return classify(tokenize(file_string));
+    return classify(tokenize(remove_spaces(file_string)));
 }
 
 // Split an expression across the relational
@@ -13,7 +13,7 @@ pub fn split(token:String) -> (String, String, String) {
     let mut relational_string:String = "".to_string();
     let mut second_part_string:String = "".to_string();
     let mut in_exp:bool = false;
-        
+    
     // Splits expression based on relational
     for c in char_vec {
 	if c == '=' || c == '<' || c == '>' || c == '!' {
@@ -36,7 +36,7 @@ pub fn split_over_op(token:String) -> (String, String, String) {
     let mut operation_string:String = "".to_string();
     let mut second_part_string:String = "".to_string();
     let mut in_exp:bool = false;
-        
+    
     // Splits expression based on operation
     for c in char_vec {
 	if (c == '+' || c == '/' || c == '*' || c == '-') && !in_exp {
@@ -56,11 +56,18 @@ pub fn split_over_op(token:String) -> (String, String, String) {
 fn remove_spaces(file_string:String) -> String {
     let char_vec:Vec<char> = file_string.chars().collect();
     let mut output_string:String = "".to_string();
-        
+    let mut in_string = false;
+    
     for c in char_vec {
 	if c != ' ' {
 	    output_string.push(c);
-        }
+        } else if in_string {
+	    output_string.push(c);
+	}
+
+	if c == '"' {
+	    in_string = !in_string;
+	}
     }
 
     return output_string;
@@ -68,43 +75,22 @@ fn remove_spaces(file_string:String) -> String {
 
 // Create a vector of tokens
 fn tokenize(file_string:String) -> Vec<String> {
-    let file_bytes = file_string.as_bytes();
-    let mut token: Vec<u8> = Vec::new();
     let mut output: Vec<String> = Vec::new();
-    let mut in_string = false;
+    let mut cur:String = file_string.trim().to_string().clone();
+    let mut offset = 0;
+    let locations = find_res_tokens(file_string);
 
-    // Step through each char
-    for i in 0..file_string.len() {
-	// Check if in string 
-	if file_bytes[i] == b'"' {
-	    in_string = !in_string;
-	}
-
-	// Add to token or finish token
-	if ((file_bytes[i] == b';') | (file_bytes[i] == b',')) && !in_string {
-	    // If we hit ; or , make two tokens and move on
-	    output.push(String::from_utf8_lossy(&token).to_string());
-	    token = Vec::new();
-
-	    // Punc token
-	    token.push(file_bytes[i]);
-	    output.push(String::from_utf8_lossy(&token).to_string());
-	    token = Vec::new();	    
-	} else if (file_bytes[i] != b' ' && file_bytes[i] != b'\n') | in_string {
-	    // Push char to token
-	    token.push(file_bytes[i]);
-	} else if (token.len() > 0) && !in_string {
-	    // Push token to vector and make new token
-	    output.push(String::from_utf8_lossy(&token).to_string());
-	    token = Vec::new();
-	}
+    for i in locations {
+	let (chunk, rest) = cur.split_at(i - offset);
+	offset = i;
+	output.push(chunk.to_string());
+	cur = rest.to_string();
     }
 
-    // If we have a stray token, push it
-    if token.len() > 0 {
-	output.push(String::from_utf8_lossy(&token).to_string());
+    if cur != "".to_string() {
+	output.push(cur);
     }
-
+    
     return output;
 }
 
@@ -117,7 +103,7 @@ fn classify(tokens:Vec<String>) -> (Vec<String>, Vec<String>) {
     // Find each token
     for t in tokens {
 	// Check if the line number has been seen
-	if !line_set {
+	if !line_set && find_token(t.clone()) == "int".to_string() {
 	    // Classify line_num
 	    text.push(t.clone());
 	    class.push("line_num".to_string());
@@ -129,7 +115,56 @@ fn classify(tokens:Vec<String>) -> (Vec<String>, Vec<String>) {
 	}
     }
 
+    if class[0] != "line_num".to_string() {
+	panic!("LEXER: classify: Line does not have a line number");
+    }
+
     return (text, class);
+}
+
+// Find the beginnings and ends of all matching reserved tokens
+fn find_res_tokens(file_string:String) -> Vec<usize> {
+    let reserved_tokens:Vec<String> = vec!["IF".to_string(), "THEN".to_string(),
+					   "GOTO".to_string(), "FOR".to_string(),
+					   ";".to_string(), ",".to_string(),
+					   "NEXT".to_string(), "REM".to_string(),
+					   "RETURN".to_string(), "GOSUB".to_string(),
+					   "PRINT".to_string(), "LET".to_string(),
+					   "DIM".to_string(), "INPUT".to_string(),
+					   "READ".to_string(), "DATA".to_string(),
+					   "END".to_string(), "STOP".to_string()];
+    let mut locations:Vec<usize> = Vec::new();
+    let mut i_in_string = Vec::new();
+    let mut in_string = false;
+    let mut i = 0;
+    let char_vec:Vec<char> = file_string.chars().collect();
+    
+    for c in char_vec {
+	if c == '"' {
+	    in_string = !in_string;
+	}
+
+	if in_string {
+	    i_in_string.push(i);
+        }
+
+	i = i + 1;
+    }
+    
+    for i in reserved_tokens {
+	let value:Vec<_> = file_string.match_indices(&i).map(|(j, _)|j).collect();
+	
+	for loc in value {
+	    if !i_in_string.contains(&loc) {
+		locations.push(loc);
+		locations.push(loc + i.len());
+	    }
+	}
+    }
+
+    locations.sort();
+    
+    return locations;
 }
 
 // Classify token
@@ -220,13 +255,14 @@ pub fn is_string(token:String) -> bool {
 fn is_res(token:String) -> bool {
     let reserved_tokens:Vec<String> = vec!["IF".to_string(), "THEN".to_string(),
 					   "GOTO".to_string(), "FOR".to_string(),
-					   "TO".to_string(), "NEXT".to_string(),
+					   ";".to_string(), ",".to_string(),
+					   "NEXT".to_string(), "REM".to_string(),
 					   "RETURN".to_string(), "GOSUB".to_string(),
 					   "PRINT".to_string(), "LET".to_string(),
 					   "DIM".to_string(), "INPUT".to_string(),
 					   "READ".to_string(), "DATA".to_string(),
-					   "END".to_string()];
-
+					   "END".to_string(), "STOP".to_string()];
+    
     // Check if token is one of the reserved_tokens
     if reserved_tokens.contains(&token) {
 	return true;
@@ -240,7 +276,52 @@ fn is_res(token:String) -> bool {
 mod test {
     // File Imports
     use lexer::*;
+
+    // Testing tokenize()
+    #[test]
+    fn tokenize_1() {
+	let given:String = remove_spaces("30000 REM This is just some random test lol".to_string());
+	let answer:Vec<String> = vec!["30000".to_string(), "REM".to_string(), "Thisisjustsomerandomtestlol".to_string()];
+
+	assert_eq!(answer, tokenize(given));
+    }
     
+    // Testing tokenize()
+    #[test]
+    fn tokenize_2() {
+	let given:String = remove_spaces("10 STOP".to_string());
+	let answer:Vec<String> = vec!["10".to_string(), "STOP".to_string()];
+
+	assert_eq!(answer, tokenize(given));
+    }    
+
+    // Testing tokenize()
+    #[test]
+    fn tokenize_3() {
+	let given:String = remove_spaces("100 LET A=\"Fuh\"".to_string());
+	let answer:Vec<String> = vec!["100".to_string(), "LET".to_string(), "A=\"Fuh\"".to_string()];
+
+	assert_eq!(answer, tokenize(given));
+    }
+    
+    // Testing tokenize()
+    #[test]
+    fn tokenize_4() {
+	let given:String = remove_spaces("2000 PRINT A;B;C;".to_string());
+	let answer:Vec<String> = vec!["2000".to_string(), "PRINT".to_string(), "A".to_string(), ";".to_string(), "B".to_string(), ";".to_string(), "C".to_string(), ";".to_string()];
+
+	assert_eq!(answer, tokenize(given));
+    }
+
+    // Testing tokenize()
+    #[test]
+    fn tokenize_5() {
+	let given:String = remove_spaces("100 LET A=\"Fuh RETURN TO LET STOP GOTO GOSUB\"".to_string());
+	let answer:Vec<String> = vec!["100".to_string(), "LET".to_string(), "A=\"Fuh RETURN TO LET STOP GOTO GOSUB\"".to_string()];
+
+	assert_eq!(answer, tokenize(given));
+    }
+        
     // Testing split()
     #[test]
     fn split_1() {
@@ -262,7 +343,7 @@ mod test {
     // Testing remove_spaces()
     #[test]
     fn remove_spaces_1() {
-	let given:String = "A =\"F u h\"".to_string();
+	let given:String = "A =\"Fuh\"".to_string();
 	let answer:String = "A=\"Fuh\"".to_string();
 
 	assert_eq!(answer, remove_spaces(given));
@@ -273,6 +354,15 @@ mod test {
     fn remove_spaces_2() {
 	let given:String = "                  ".to_string();
 	let answer:String = "".to_string();
+
+	assert_eq!(answer, remove_spaces(given));
+    }
+
+    // Testing remove_spaces()
+    #[test]
+    fn remove_spaces_3() {
+	let given:String = "     1 0 0 P R  I NT\" Fuh Foo  Fin \"".to_string();
+	let answer:String = "100PRINT\" Fuh Foo  Fin \"".to_string();
 
 	assert_eq!(answer, remove_spaces(given));
     }
