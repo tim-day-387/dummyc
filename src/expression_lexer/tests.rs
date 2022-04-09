93ba1c39610f4c41c69e1c8993eab094c683ed9e
Tim Day <tday141@gmail.com>
2022-04-08 23:35:11 -0400

    Added unit tests for is_* functions

--- src/expression_lexer/tests.rs
+++ src/expression_lexer/tests.rs
@@ -18,3 +18,57 @@ fn split_2() {
 
     assert_eq!(answer, split(given, true));
 }
+
+// Testing is_float()
+#[test]
+fn is_float_1() {
+    let given:String = "3.14159".to_string();
+    let answer = true;
+
+    assert_eq!(answer, _is_float(given));
+}
+
+// Testing is_float()
+#[test]
+fn is_float_2() {
+    let given:String = "3.1sdfsdf4159".to_string();
+    let answer = false;
+
+    assert_eq!(answer, _is_float(given));
+}
+
+// Testing is_int()
+#[test]
+fn is_int_1() {
+    let given:String = "387".to_string();
+    let answer = true;
+
+    assert_eq!(answer, is_int(given));
+}
+
+// Testing is_int()
+#[test]
+fn is_int_2() {
+    let given:String = "stringlmao".to_string();
+    let answer = false;
+
+    assert_eq!(answer, is_int(given));
+}
+
+// Testing is_string()
+#[test]
+fn is_string_1() {
+    let given:String = "\"This is a test\"".to_string();
+    let answer = true;
+
+    assert_eq!(answer, is_string(given));
+}
+
+// Testing is_string()
+#[test]
+fn is_string_2() {
+    let given:String = "\"This is \"\" a test\"".to_string();
+    let answer = false;
+
+    assert_eq!(answer, is_string(given));
+}
