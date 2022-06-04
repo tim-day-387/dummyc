a28f45c0edad116b9142c231004e46c8e7f75194
Tim Day <tday141@gmail.com>
2022-06-04 01:33:42 -0400

    Remove panics from main

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,11 @@ fn main() {
 	    basic_path = Path::new(&args[n]);
 	    path_set = true;
 	} else {
-	    panic!("File path already specified!");
+	    let artifacts = [].to_vec();
+	    let artifact_names = [].to_vec();
+	    let function_name = "main".to_string();
+	    let message = "File path already specified.".to_string();
+	    stateless_error(artifacts, artifact_names, function_name, message);
 	}
     }
 
