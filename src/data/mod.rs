90424cb5ed5e2dd120a07fc442bb5abcaf5e68ca
Tim Day <tday141@gmail.com>
2022-10-09 00:11:57 -0400

    Fix clippy recommendations in data

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -37,14 +37,14 @@ impl Data {
 
 	output.simplify(state);
 
-	if given_text.clone() != "".to_string() && output.plain_text.clone() == "".to_string() {
-	    stateless_error([given_text.clone()].to_vec(),
+	if given_text != *"" && output.plain_text.clone() == *"" {
+	    stateless_error([given_text].to_vec(),
 			    ["token".to_string()].to_vec(),
 			    "new_simplified".to_string(),
 			    "Cannot reduce to empty data object.".to_string());
 	}
 
-	return output;
+	output
     }
 
 
@@ -62,12 +62,12 @@ impl Data {
     pub fn simplify(&mut self, state:State) {
 	self.find_output_type();
 	
-	if self.output_type == 2000 { // symbol_callable
-	    self.resolve_callable(state.clone());
-	} else if self.output_type == 1000 { // symbol
+	if self.output_type == 2000 {             // symbol_callable
+	    self.resolve_callable(state);
+	} else if self.output_type == 1000 {      // symbol
 	    self.resolve_symbol(state);
-	} else if self.output_type == 0 { // expression
-	    self.resolve_expression(state.clone());
+	} else if self.output_type == 0 {         // expression
+	    self.resolve_expression(state);
 	}
 	
 	self.get_print_out();
@@ -77,12 +77,12 @@ impl Data {
     // Resolve array reference in actual var name
     pub fn get_array_reference(given:String, state:State) -> String {
 	let name = split_function(given.clone()).0.to_lowercase();
-	let arguments = split_arguments(split_function(given.clone()).1);
+	let arguments = split_arguments(split_function(given).1);
 	let location;
 	let mut text = "".to_string();
 
 	if arguments.len() == 1 {
-	    let location_string = Data::new_simplified(arguments[0].clone(), state.clone()).plain_text;
+	    let location_string = Data::new_simplified(arguments[0].clone(), state).plain_text;
 
 	    location = match location_string.parse::<i64>() {
 		Ok(i) => i,
@@ -92,7 +92,7 @@ impl Data {
 	    text = format!("{}{}{}{}", name, "(", location, ")");
 	}
 
-	return text;
+	text
     }
 
 
@@ -102,14 +102,14 @@ impl Data {
 	let arguments = split_arguments(split_function(self.plain_text.clone()).1);
 	let array_ref = Data::get_array_reference(self.plain_text.clone(), state.clone());
 
-	if name == "int".to_string() && arguments.len() == 1 {
+	if name == *"int" && arguments.len() == 1 {
 	    let arg_data = Data::new_simplified(arguments[0].clone(), state.clone());
 	    let number:i64 = parse_float(arg_data.plain_text, "resolve_callable".to_string()).round() as i64;
 
 	    *self = Data::new_simplified(number.to_string(), state);
 	} else if Data::does_var_exist(array_ref.clone(), state.clone()) {
-	    self.plain_text = array_ref.clone();
-	    self.get_var_value(state.clone());
+	    self.plain_text = array_ref;
+	    self.get_var_value(state);
 	} else {
 	    self.function(state, name, arguments);
 	}
@@ -118,13 +118,13 @@ impl Data {
 
     // Resolve symbol type data
     fn resolve_symbol(&mut self, state:State) {
-	if self.plain_text.to_lowercase() == "rnd".to_string() {
+	if self.plain_text.to_lowercase() == *"rnd" {
 	    let mut rng = rand::thread_rng();
 	    let number:f64 = rng.gen();
 	    *self = Data::new_simplified(number.to_string(), state);
-	} else if self.plain_text.to_lowercase() == "prloc".to_string() {
+	} else if self.plain_text.to_lowercase() == *"prloc" {
 	    self.plain_text = state.print_location.to_string();
-	    self.simplify(state.clone());
+	    self.simplify(state);
 	} else {
 	    self.get_var_value(state);
 	}
@@ -135,21 +135,23 @@ impl Data {
     fn function(&mut self, state:State, name:String, arguments:Vec<String>) {
 	let location_a = "./std/".to_string();
 	let location_b = "/usr/lib/dummyc/std/".to_string();
-	let string_path_a = format!("{}{}{}", location_a, name, ".bas".to_string());
-	let string_path_b = format!("{}{}{}", location_b, name, ".bas".to_string());
+	let string_path_a = format!("{}{}{}", location_a, name, ".bas");
+	let string_path_b = format!("{}{}{}", location_b, name, ".bas");
 	let file_path_a = Path::new(&string_path_a);
 	let file_path_b = Path::new(&string_path_b);
-	let file_path:&Path;
 
-	if file_path_a.exists() {file_path = file_path_a;}
-	else {file_path = file_path_b;}
+	let file_path:&Path = if file_path_a.exists() {
+	    file_path_a
+	} else {
+	    file_path_b
+	};
 
 	// Useful variables
 	let mut lim_state = State::new();
 	lim_state.print_location = state.print_location;
 
 	// Add arguments
-	for args in arguments.clone() {
+	for args in arguments {
 	    let data = Data::new_simplified(args, state.clone());
 
 	    lim_state.input_args.insert(0, data);
@@ -187,9 +189,9 @@ impl Data {
 			    [].to_vec(),
 			    "find_operation_output_type".to_string(),
 			    "Incompatible types.".to_string());
-	    return -1;
+	    -1
 	} else {
-	    return cmp::max(self.output_type, other.output_type);
+	    cmp::max(self.output_type, other.output_type)
 	}
     }
 
@@ -199,22 +201,22 @@ impl Data {
 	let output_type = self.clone().find_operation_output_type(other.clone());
 
 	if output_type == 4001 || output_type == 4002 || output_type == 4003 { // int or float or sci_float
-	    let a = parse_float(self.plain_text.clone(), "compare".to_string());
-	    let b = parse_float(other.plain_text.clone(), "compare".to_string());
+	    let a = parse_float(self.plain_text, "compare".to_string());
+	    let b = parse_float(other.plain_text, "compare".to_string());
 
-	    if operation_string == "<".to_string() {
-		return a < b;
-	    } else if operation_string == ">".to_string() {
-		return a > b;
-	    } else if operation_string == ">=".to_string() {
-		return a >= b;
-	    } else if operation_string == "<=".to_string() {
-		return a <= b;
+	    if operation_string == *"<" {
+		a < b
+	    } else if operation_string == *">" {
+		a > b
+	    } else if operation_string == *">=" {
+		a >= b
+	    } else if operation_string == *"<=" {
+		a <= b
 	    } else {
-		return false;
+		false
             }
 	} else {
-	    return false;
+	    false
 	}
     }
 
@@ -224,24 +226,24 @@ impl Data {
 	let output_type:i64 = self.clone().find_operation_output_type(other.clone());
 
 	if output_type == 3000 { // string
-	    self.plain_text = format!("{}{}{}{}", "\"".to_string(), self.print_out_text.clone(), other.print_out_text.clone(), "\"".to_string());
+	    self.plain_text = format!("{}{}{}{}", "\"", self.print_out_text.clone(), other.print_out_text, "\"");
 	} else if output_type == 4001 { // int
 	    let a = parse_int(self.plain_text.clone(), "operation".to_string());
-	    let b = parse_int(other.plain_text.clone(), "operation".to_string());
+	    let b = parse_int(other.plain_text, "operation".to_string());
 
-	    if operation_string == "+".to_string() {
+	    if operation_string == *"+" {
 		self.plain_text = (a+b).to_string();
-	    } else if operation_string == "*".to_string() {
+	    } else if operation_string == *"*" {
 		self.plain_text = (a*b).to_string();
-	    } else if operation_string == "-".to_string() {
+	    } else if operation_string == *"-" {
 		self.plain_text = (a-b).to_string();
-	    } else if operation_string == "/".to_string() {
+	    } else if operation_string == *"/" {
 		if b == 0 {
 		    error_divide_zero("operation".to_string());
 		} else {
 		    self.plain_text = (a/b).to_string();
 		}
-	    } else if operation_string == "^".to_string() {
+	    } else if operation_string == *"^" {
 		self.plain_text = (a as f64).powf(b as f64).to_string();
 	    } else {
 		stateless_error([].to_vec(),
@@ -251,21 +253,21 @@ impl Data {
             } 
 	} else if output_type == 4002 || output_type == 4003 { // float or sci_float
 	    let a = parse_float(self.plain_text.clone(), "operation".to_string());
-	    let b = parse_float(other.plain_text.clone(), "operation".to_string());
+	    let b = parse_float(other.plain_text, "operation".to_string());
 
-	    if operation_string == "+".to_string() {
+	    if operation_string == *"+" {
 		self.plain_text = (a+b).to_string();
-	    } else if operation_string == "*".to_string() {
+	    } else if operation_string == *"*" {
 		self.plain_text = (a*b).to_string();
-	    } else if operation_string == "-".to_string() {
+	    } else if operation_string == *"-" {
 		self.plain_text = (a-b).to_string();
-	    } else if operation_string == "/".to_string() {
+	    } else if operation_string == *"/" {
 		if b == 0.0 {
 		    error_divide_zero("operation".to_string());
 		} else {
 		    self.plain_text = (a/b).to_string();
 		}
-	    } else if operation_string == "^".to_string() {
+	    } else if operation_string == *"^" {
 		self.plain_text = a.powf(b).to_string();
 	    } else {
 		stateless_error([].to_vec(),
@@ -308,10 +310,7 @@ impl Data {
 
     // Does a variable by that name exist?
     fn does_var_exist(name:String, state:State) -> bool {
-	match state.variables.get(&name) {
-	    Some(_value)=> return true,
-	    _=> return false,
-	}
+	matches!(state.variables.get(&name), Some(_value))
     }
 
 
@@ -325,39 +324,38 @@ impl Data {
 	    let i = parse_int(self.plain_text.clone(), "get_print_out".to_string());
 
 	    if i < 0 {
-		self.print_out_text = format!("{}{}", i, " ".to_string());
+		self.print_out_text = format!("{}{}", i, " ");
 	    } else {
-		self.print_out_text = format!("{}{}{}", " ".to_string(), i, " ".to_string());
+		self.print_out_text = format!("{}{}{}", " ", i, " ");
 	    }
 	} else if self.output_type == 4002 { // float
 	    let i = parse_float(self.plain_text.clone(), "get_print_out".to_string());
 
 	    if i < -1.0 {
-		self.print_out_text = format!("{}{}", i, " ".to_string());
+		self.print_out_text = format!("{}{}", i, " ");
 	    } else if i < 0.0 {
-		self.print_out_text = format!("{}{}", i, " ".to_string());
+		self.print_out_text = format!("{}{}", i, " ");
 		self.print_out_text.remove(1);
 	    } else if i == 0.0 {
-		self.print_out_text = format!("{}{}{}", " ".to_string(), i.abs(), " ".to_string());
+		self.print_out_text = format!("{}{}{}", " ", i.abs(), " ");
 	    } else if i < 1.0 {
-		self.print_out_text = format!("{}{}{}", " ".to_string(), i, " ".to_string());
+		self.print_out_text = format!("{}{}{}", " ", i, " ");
 		self.print_out_text.remove(1);
 	    } else {
-		self.print_out_text = format!("{}{}{}", " ".to_string(), i, " ".to_string());
+		self.print_out_text = format!("{}{}{}", " ", i, " ");
 	    }
 	} else if self.output_type == 4003 { // sci_float
 	    let mut output:String = "".to_string();
 	    let mut last = ' ';
 	    let mut point = false;
-	    let temp;
 
 	    let i = parse_float(self.plain_text.clone(), "get_print_out".to_string());
 
-	    if i < 0.0 {
-		temp = format!("{:.E}{}", i, " ".to_string());
+	    let temp = if i < 0.0 {
+		format!("{:.E}{}", i, " ")
 	    } else {
-		temp = format!("{}{:.E}{}", " ".to_string(), i, " ".to_string());
-	    }
+		format!("{}{:.E}{}", " ", i, " ")
+	    };
 
 	    for c in temp.chars() {
 		if c == '.' {point = true;}
